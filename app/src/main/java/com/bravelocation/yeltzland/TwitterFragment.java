6ab5c24e6bb618cf26135829f98be521ce2f5d23
John Pollard <johnp@bravelocation.com>
2016-05-27 13:29:45 +0100

    Added swift to refresh on Twitter view

--- app/src/main/java/com/bravelocation/yeltzland/TwitterFragment.java
+++ app/src/main/java/com/bravelocation/yeltzland/TwitterFragment.java
@@ -2,6 +2,7 @@ package com.bravelocation.yeltzland;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,14 @@ import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TimelineResult;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
 public class TwitterFragment extends ListFragment {
+
+    private TweetTimelineListAdapter adapter;
 
     public TwitterFragment() {
         // Required empty public constructor
@@ -26,28 +30,49 @@ public class TwitterFragment extends ListFragment {
         super.onCreate(savedInstanceState);
 
         final UserTimeline userTimeline = new UserTimeline.Builder().screenName("halesowentownfc").build();
-        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getActivity())
+        this.adapter = new TweetTimelineListAdapter.Builder(getActivity())
                 .setTimeline(userTimeline)
                 .build();
-        setListAdapter(adapter);
+        setListAdapter(this.adapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_twitter, container, false);
+        View view = inflater.inflate(R.layout.fragment_twitter, container, false);
+
+        final SwipeRefreshLayout swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
+
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeLayout.setRefreshing(true);
+                adapter.refresh(new Callback<TimelineResult<Tweet>>() {
+                    @Override
+                    public void success(Result<TimelineResult<Tweet>> result) {
+                        swipeLayout.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        // Toast or some other action
+                    }
+                });
+            }
+        });
+
+        return view;
     }
 
     public void reload() {
         TweetTimelineListAdapter adapter = (TweetTimelineListAdapter) this.getListAdapter();
         adapter.refresh(new Callback<TimelineResult<Tweet>>() {
-            // Do nothing on failure for now
+            // Do nothing for now
             @Override
             public void success(Result<TimelineResult<Tweet>> result) {}
 
             @Override
             public void failure(TwitterException exception) {}
-
         });
     }
 }
