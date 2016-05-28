d1567d5d0a0911e414fb71ed8196bda10a08aecb
John Pollard <johnp@bravelocation.com>
2016-05-28 08:53:55 +0100

    Improved Twitter refresh

--- app/src/main/java/com/bravelocation/yeltzland/TwitterFragment.java
+++ app/src/main/java/com/bravelocation/yeltzland/TwitterFragment.java
@@ -1,18 +1,18 @@
 package com.bravelocation.yeltzland;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ListFragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
 
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TimelineResult;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
@@ -20,6 +20,21 @@ import com.twitter.sdk.android.tweetui.UserTimeline;
 public class TwitterFragment extends ListFragment {
 
     private TweetTimelineListAdapter adapter;
+    private SwipeRefreshLayout swipeLayout;
+    private Handler refreshHandler;
+    private final int RELOAD_INTERVAL = 1000 * 60 * 5;
+    private final Callback<TimelineResult<Tweet>> timelineRefreshCallback = new Callback<TimelineResult<Tweet>>() {
+        // Do nothing for now
+        @Override
+        public void success(Result<TimelineResult<Tweet>> result) {
+            swipeLayout.setRefreshing(false);
+        }
+
+        @Override
+        public void failure(TwitterException exception) {
+            swipeLayout.setRefreshing(false);
+        }
+    };
 
     public TwitterFragment() {
         // Required empty public constructor
@@ -33,7 +48,9 @@ public class TwitterFragment extends ListFragment {
         this.adapter = new TweetTimelineListAdapter.Builder(getActivity())
                 .setTimeline(userTimeline)
                 .build();
-        setListAdapter(this.adapter);
+        this.setListAdapter(this.adapter);
+
+        this.setupTimedReload();
     }
 
     @Override
@@ -41,38 +58,49 @@ public class TwitterFragment extends ListFragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_twitter, container, false);
 
-        final SwipeRefreshLayout swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
-
-        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        this.swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
+        this.swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                swipeLayout.setRefreshing(true);
-                adapter.refresh(new Callback<TimelineResult<Tweet>>() {
-                    @Override
-                    public void success(Result<TimelineResult<Tweet>> result) {
-                        swipeLayout.setRefreshing(false);
-                    }
-
-                    @Override
-                    public void failure(TwitterException exception) {
-                        // Toast or some other action
-                    }
-                });
+                reload();
             }
         });
 
         return view;
     }
 
+    // Reload action - either reload button or swipe
     public void reload() {
-        TweetTimelineListAdapter adapter = (TweetTimelineListAdapter) this.getListAdapter();
-        adapter.refresh(new Callback<TimelineResult<Tweet>>() {
-            // Do nothing for now
-            @Override
-            public void success(Result<TimelineResult<Tweet>> result) {}
-
-            @Override
-            public void failure(TwitterException exception) {}
-        });
+        this.swipeLayout.setRefreshing(true);
+        this.reloadTimeline();
+        this.resetTimedReload();
     }
+
+    // Actually reload timeline
+    private void reloadTimeline() {
+        Log.d("Reload", "Reloading timeline");
+        this.adapter.refresh(this.timelineRefreshCallback);
+    }
+
+    // Timer refresh functions
+    private void setupTimedReload() {
+        this.refreshHandler = new Handler();
+        this.refreshHandler.postDelayed(refreshTimeline, RELOAD_INTERVAL);
+    }
+
+    private void resetTimedReload() {
+        if (this.refreshHandler != null) {
+            this.refreshHandler.removeCallbacks(this.refreshTimeline);
+        }
+
+        this.setupTimedReload();
+    }
+
+    private Runnable refreshTimeline = new Runnable() {
+        @Override
+        public void run() {
+            reloadTimeline();
+            refreshHandler.postDelayed(refreshTimeline, RELOAD_INTERVAL);
+        }
+    };
 }
