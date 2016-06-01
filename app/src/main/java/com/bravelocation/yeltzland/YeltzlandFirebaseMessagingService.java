37d6dbc874c44a22e4e18c9dc478fc38023ec65d
John Pollard <johnp@bravelocation.com>
2016-06-01 09:55:26 +0100

    Added notification support plus foreground

---
+++ app/src/main/java/com/bravelocation/yeltzland/YeltzlandFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package com.bravelocation.yeltzland;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class YeltzlandFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        final RemoteMessage.Notification notification = remoteMessage.getNotification();
+
+        // Show notification on UI thread
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), notification.getBody(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
