024f3dd29b4bf58d0dffe77bcdf0730619d40b36
John Pollard <johnp@bravelocation.com>
2017-01-21 16:18:54 +0000

    Moved Fabric init into application create

--- app/src/main/java/com/bravelocation/yeltzland/SplashActivity.java
+++ app/src/main/java/com/bravelocation/yeltzland/SplashActivity.java
@@ -4,19 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.crashlytics.android.Crashlytics;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-
-import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
-
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "mOQVcR8FPJPXw2lwncM5bNkSI";
-    private static final String TWITTER_SECRET = "wN1q1WPXs48Vc2sv5sm2f1bivwPrdUNDigy7oOpCUl0dpfvxS5";
 
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 500;
@@ -25,26 +14,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-
-        Fabric fabric = null;
-
-        if (BuildConfig.DEBUG) {
-            Log.d("Yeltzland", "DEBUG mode");
-            fabric = new Fabric.Builder(this)
-                    .kits(new Twitter(authConfig))
-                    .debuggable(true)
-                    .build();
-        } else {
-            Log.d("Yeltzland", "RELEASE mode");
-            fabric = new Fabric.Builder(this)
-                    .kits(new Crashlytics(), new Twitter(authConfig))
-                    .debuggable(false)
-                    .build();
-        }
-
-        Fabric.with(fabric);
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
--- app/src/main/java/com/bravelocation/yeltzland/YeltzlandApplication.java
+++ app/src/main/java/com/bravelocation/yeltzland/YeltzlandApplication.java
@@ -3,12 +3,43 @@ package com.bravelocation.yeltzland;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
 public class YeltzlandApplication extends MultiDexApplication
 {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "mOQVcR8FPJPXw2lwncM5bNkSI";
+    private static final String TWITTER_SECRET = "wN1q1WPXs48Vc2sv5sm2f1bivwPrdUNDigy7oOpCUl0dpfvxS5";
+
     @Override
     public void onCreate()
     {
         super.onCreate();
+
+        // Setup Fabric and Twitter
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+
+        Fabric fabric = null;
+
+        if (BuildConfig.DEBUG) {
+            Log.d("Yeltzland", "DEBUG mode");
+            fabric = new Fabric.Builder(this)
+                    .kits(new Twitter(authConfig))
+                    .debuggable(true)
+                    .build();
+        } else {
+            Log.d("Yeltzland", "RELEASE mode");
+            fabric = new Fabric.Builder(this)
+                    .kits(new Crashlytics(), new Twitter(authConfig))
+                    .debuggable(false)
+                    .build();
+        }
+
+        Fabric.with(fabric);
 
         // Setup handler for uncaught exceptions.
         Thread.setDefaultUncaughtExceptionHandler (new Thread.UncaughtExceptionHandler()
