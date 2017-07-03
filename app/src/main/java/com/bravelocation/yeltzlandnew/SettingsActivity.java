28a099a4539b676eb96e5b15543cec7891871261
John Pollard <johnp@bravelocation.com>
2017-07-03 09:00:06 +0100

    Fixed issue in settings page

--- app/src/main/java/com/bravelocation/yeltzlandnew/SettingsActivity.java
+++ app/src/main/java/com/bravelocation/yeltzlandnew/SettingsActivity.java
@@ -1,20 +1,10 @@
 package com.bravelocation.yeltzlandnew;
 
-import android.app.Activity;
-import android.graphics.PorterDuff;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-
-public class SettingsActivity extends Activity {
-    private Typeface textFont;
-
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,31 +13,21 @@ public class SettingsActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+
+        // Add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        this.textFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "american_typewriter_regular.ttf");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
 
-        super.onPostCreate(savedInstanceState);
-
-        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
-
-        Toolbar toolbar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
-        toolbar.setTitleTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
-
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_left);
-        toolbar.getNavigationIcon().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.whiteOverlay), PorterDuff.Mode.MULTIPLY);
-
-        TextView title = (TextView) toolbar.findViewById(R.id.toolbar_title);
-        title.setTypeface(this.textFont);
-
-        root.addView(toolbar, 0); // insert at top
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        return super.onOptionsItemSelected(item);
     }
 }
