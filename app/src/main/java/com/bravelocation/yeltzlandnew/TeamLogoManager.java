e9889f30506a47a9adb026635a80a441b68b2fba
John Pollard <johnp@bravelocation.com>
2020-03-11 10:50:22 +0000

    Updated to use local copy of TwitterKit

--- app/src/main/java/com/bravelocation/yeltzlandnew/TeamLogoManager.java
+++ app/src/main/java/com/bravelocation/yeltzlandnew/TeamLogoManager.java
@@ -7,7 +7,7 @@ import com.squareup.picasso.Picasso;
 public class TeamLogoManager {
     public void LoadTeamImageIntoView(Context context, String teamName, ImageView view) {
         String imageUrl = String.format("https://bravelocation.com/teamlogos/%s.png", this.makeTeamFileName(teamName));
-        Picasso.with(context).load(imageUrl).placeholder(R.drawable.blank_team).into(view);
+        Picasso.get().load(imageUrl).placeholder(R.drawable.blank_team).into(view);
     }
 
     private String makeTeamFileName(String teamName) {
